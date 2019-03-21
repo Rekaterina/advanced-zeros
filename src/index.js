85c8505fbd1b41a8580ece31fa0307457be23823
Rekaterina <rkatherine0602@gmail.com>
2019-03-22 00:24:03 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let sum = 0;
+    let result = [];
+    let num = number;
+    let primeNumbers = [];
+    let amountPrimeNumbers = [];
+    let obj = {};
+    let j = 2;
+    
+    while (base > 0 && j <= base) {
+        while (base % j == 0) {
+            primeNumbers.push(j);
+            base = base / j;
+        };
+        j++;
+    };
+   
+    primeNumbers.forEach(function(item) {
+        if (obj[item] != undefined) {
+            obj[item]++;
+        } else {
+            obj[item] = 1;
+        }
+    });
+     
+    for (let key in obj) {
+        amountPrimeNumbers.push([+key, obj[key]]);
+    };
+    
+    for (let i = 0; i < amountPrimeNumbers.length; i++) {
+        while (number > 0) {
+            number = Math.floor(number / amountPrimeNumbers[i][0]);
+            sum += number;
+        };
+        result[i] = Math.floor(sum / amountPrimeNumbers[i][1]);
+        sum = 0;
+        number = num;
+    }
+
+    return Math.min(...result);
 }
